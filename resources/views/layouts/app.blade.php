c88389bbdb1fc120b06a3d235d5a313fcf6c294d
Carlos <carlosdavidmedinarangel1007@gmail.com>
2023-03-12 17:24:07 -0400

    Añadido el bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('titulo')</title>
+    @vite(['resources/css/app.scss' , 'resources/js/app.js'])
 </head>
 <body>
     @include('partials.nav')
 
     @yield('content')
+
+    
 </body>
 </html>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,7 +1,31 @@
-<ul>
-    <li><a href="/">Inicio</a></li>
-    <li><a href="{{ route('about') }}">Nosotros</a></li>
-    <li><a href="{{ route('instruc') }}">Instrucciones</a></li>
-    <li><a href="{{ route('registro') }}">Registro</a></li>
-    <li><a href="{{ route('login') }}">Login</a></li>
-</ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+
+        <img src="{{ asset('resources/img/triangulo.png') }}" alt="Inf">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('about') }}">Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('instruc') }}">Instrucciones</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('registro') }}">Registro</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">Login</a>
+          </li>
+         
+        </ul>
+      </div>
+    </div>
+  </nav>
